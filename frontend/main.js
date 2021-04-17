caa1261566004d4fe3ab4f6195ff3ce46c363b2d
Guido Percú <guidopercu@gmail.com>
2021-04-17 17:50:51 -0300

    Add material design to frontend

--- frontend/main.js
+++ frontend/main.js
@@ -3,23 +3,26 @@ const App = {
       return {
         category: "Sandman",
         playlist: null,
-        artist: null,
-        song: "",
-        lyrics: "",
       }
     },
+    mounted () {
+        axios
+          .get('http://127.0.0.1:5000//track/search/' + this.category)
+          .then(response => (this.playlist = response.data.musics))
+      },
     methods: {
         generatePlaylist() {
             axios
             .get('http://127.0.0.1:5000//track/search/' + this.category)
-            .then(function(response) {
+            .then(response => (this.playlist = response.data.musics))
+            /*.then(function(response) {
                 this.playlist = response.data.track
                 this.artist = response.data.track.artist_name
                 this.song = response.data.track.track_name
                 this.lyrics = response.data.track.lyrics
 
                 console.log(this.playlist)
-            })
+            })*/
         }
     }
   }
