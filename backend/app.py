a0921fd5d237bf1ef3a56e4ba5bd6770282db5d8
Guido Percú <guidopercu@gmail.com>
2021-04-17 15:26:30 -0300

    Add first request to external API

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import requests
+import os
+
 app = Flask(__name__)
+
+BASE_MUSIC_URL = "https://api.musixmatch.com/ws/1.1/"
+MUSIX_API_KEY = os.environ.get('MUSIX_API_KEY')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/track/search/<lyrics>')
+def search(lyrics: str) -> str:
+    base_request = "track.search?format=jsonp&callback=callback&quorum_factor=1&apikey={}&q_lyrics".format(MUSIX_API_KEY)
+    r = requests.get("{}{}".format(BASE_MUSIC_URL, base_request, lyrics))
+    return r.content
