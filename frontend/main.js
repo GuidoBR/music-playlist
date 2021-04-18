90bba89a21968b0ad011757b13594d22f3ba6833
Guido Percú <guidopercu@gmail.com>
2021-04-18 17:28:52 -0300

    Add randomness logic to frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,9 @@
 const App = {
+    el: '#app',
     data() {
       return {
         category: "Sandman",
-        playlist: null,
+        playlist: [],
       }
     },
     mounted () {
@@ -12,11 +13,34 @@ const App = {
     },
     methods: {
         generatePlaylist() {
-            console.log(this.playlist);
+            new_first_song = this.playlist[1]
+            lyrics = new_first_song.lyrics
+            this.category = this.selectRandomWord(lyrics)
+            console.log(this.category)
+            this.playlist = [new_first_song]
+            
             axios
             .get('http://127.0.0.1:5000//track/search/' + this.category)
-            .then(response => (this.playlist = response.data.musics))
-        }
+            .then(response => (this.playlist[1] = response.data.musics[1]))
+        },
+        selectRandomWord(lyrics) {
+            // Remove punctuation from string
+            lyrics = lyrics.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+            
+            // Break string by new lines
+            words_from_song = lyrics.split("\n", 20)
+            
+            // Remove empty spaces
+            var words_from_song = words_from_song.filter(function (el) {
+                return el != "";
+            });
+
+            // remove copyright and number at the end of lyrics
+            words_from_song.splice(-2) 
+
+            console.log(words_from_song)
+            return words_from_song[Math.floor(Math.random() * words_from_song.length)]
+        },
     }
   }
   
